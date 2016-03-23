643f7c7bf058a11ed94be19841831dd89669dc77
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2016-03-23 00:43:28 -0300

    Adding new routes and refactoring app.js

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 var express = require('express');
 var app = express();
 
-app.get('/digimons', function (req, res) {
-	res.json([
-  	{ "name": "Agumon (アグモン)" },
-  	{ "level": "Rookie" },
-  	{ "type": "Reptile" },
-  	{ "attribute": "Vaccine" },
-	]);
+app.get('/digimons', function(req, res) {
+	res.json([{
+		"name": "Agumon (アグモン)"
+	}, {
+		"level": "Rookie"
+	}, {
+		"type": "Reptile"
+	}, {
+		"attribute": "Vaccine"
+	}, ]);
 });
 
-app.get('/digimons/:_id', function (req, res) {
-  res.json([
-  	{ "name": "Rodrigo Oler" }
-	]);
+app.get('/digimons/:_id', function(req, res) {
+	var _id = parseInt(req.params._id, 10);
+	res.json([{
+		"name": "Rodrigo Oler"
+	}]);	
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.post('/digimons/', function(req, res) {
+	r
+});
+
+app.listen(3000, function() {
+	console.log('Example app listening on port 3000!');
 });
