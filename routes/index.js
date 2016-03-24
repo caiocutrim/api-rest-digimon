e407710f71edf6da70e5892bf32cd6185ab183bd
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2016-03-23 23:04:02 -0300

    Implementing routes and refactoring.

--- routes/index.js
+++ routes/index.js
@@ -2,39 +2,10 @@ var express = require('express');
 var router = express.Router();
 var DigimonsController = new require('../controllers/DigimonsController');
 
-router.get('/digimons', function(req, res) {
-	res.json([{
-		"name": "Agumon (アグモン)"
-	}, {
-		"level": "Rookie"
-	}, {
-		"type": "Reptile"
-	}, {
-		"attribute": "Vaccine"
-	}, ]);
-});
-
-router.get('/digimons/:_id', function(req, res) {
-	var _id = parseInt(req.params._id, 10);
-	res.json([{
-		"name": "Rodrigo Oler"
-	}]);
-});
-
-router.post('/digimons', DigimonsController.create.bind(DigimonsController));
-
-router.put('/digimons/:_id', function(req, res) {
-	var _id = parseInt(req.params, 10);
-	res.json({
-		"status": "Updated"
-	});
-});
-
-router.delete('/digimons/:_id', function(req, res) {
-	var _id = parseInt(req.params, 10);
-	res.json({
-		"status": "Deleted"
-	});
-});
+router.get('/digimons', 		DigimonsController.findAll.bind(DigimonsController));
+router.get('/digimons/:_id', 	DigimonsController.findOne.bind(DigimonsController));
+router.post('/digimons', 		DigimonsController.create.bind(DigimonsController));
+router.put('/digimons/:_id', 	DigimonsController.update.bind(DigimonsController));
+router.delete('/digimons/:_id', DigimonsController.delete.bind(DigimonsController));
 
 module.exports = router;
